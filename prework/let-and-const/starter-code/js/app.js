167a869240ee0a931cfecb3b9813e52b87a1ac8e
Hadeel H Al-Hawajreh <70091170+hadeelhhawajreh@users.noreply.github.com>
2020-10-16 22:34:56 +0300

    Update app.js

--- prework/let-and-const/starter-code/js/app.js
+++ prework/let-and-const/starter-code/js/app.js
@@ -37,7 +37,7 @@ function displayPics(){
   }
   console.log(rando);
   // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `var to `let`.
-  // PUT YOUR RESPONSE IN THIS COMMENT
+  // because the let is lock scope
   console.log(viewed);
 
   for (let i = 0; i < 3; i++){
